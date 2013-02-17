8f905e899fe8e1429488ef7b2ca8d889f7be076b
mzywitza <markus.zywitza@gmail.com>
2013-02-18 00:15:01 +0100

    DataTables added as grid

--- EmpireMap/Controllers/ApplicationController.cs
+++ EmpireMap/Controllers/ApplicationController.cs
@@ -1,6 +1,7 @@
 ﻿using EmpireMap.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,11 +15,23 @@ namespace EmpireMap.Controllers
     {
         ApplicationContext ctx = new ApplicationContext(); 
 
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
             if (!User.IsInRole("Benutzer"))
                 return RedirectToAction("Index","Player");
-            return View();
+
+            var map = id.HasValue ?
+                ctx.Maps.Single(m => m.MapId == id.Value) :
+                ctx.Maps.Single(m => m.Name == "Grün");
+
+            var model = new MapCastleModel
+            {
+                Id = map.MapId,
+                Maps = ctx.Maps.ToList(),
+                Castles = ctx.Castles.Where(c => c.MapId == map.MapId).Include(c => c.Player).ToList()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ EmpireMap/Models/MapCastleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpireMap.Models
+{
+    public class MapCastleModel
+    {
+        public int Id { get; set; }
+        public List<Map> Maps { get; set; }
+        public List<Castle> Castles { get; set; }
+    }
+}
