82daa8d7f803c540f0d40418c77a54653fa6914e
mzywitza <markus.zywitza@gmail.com>
2013-02-23 13:15:48 +0100

    Map tweaked by customizing gist 1182434

--- EmpireMap/Models/MapCastleModel.cs
+++ EmpireMap/Models/MapCastleModel.cs
@@ -10,5 +10,17 @@ namespace EmpireMap.Models
         public int Id { get; set; }
         public List<Map> Maps { get; set; }
         public List<Castle> Castles { get; set; }
+        
+        public string MapName
+        {
+            get
+            {
+                var defName = "unbekannt...";
+                if (Maps == null) return defName;
+                var map = Maps.SingleOrDefault(m => m.MapId == Id);
+                if (map == null) return defName;
+                return map.Name;
+            }
+        }
     }
 }
